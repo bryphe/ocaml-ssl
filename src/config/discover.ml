bdefb0277ce303c717f24eb87065078684a60aa1
Bryan Phelps <bryphe@outlook.com>
2018-10-10 12:03:14 -0700

    Update lockfile

--- src/config/discover.ml
+++ src/config/discover.ml
@@ -1,10 +1,10 @@
 module C = Configurator.V1
 
-let () =
-  C.main ~name:"ssl" (fun _c ->
-      let libPath = Sys.getenv "OPENSSL_LIB_PATH" in
-      let includePath = Sys.getenv "OPENSSL_INCLUDE_PATH" in
-      let libs = ["-L" ^ libPath; "-lssl"; "-lcrypto"] in
-      let cflags = ["-I" ^ includePath] in
-      C.Flags.write_sexp "c_library_flags.sexp" libs;
-      C.Flags.write_sexp "c_flags.sexp" cflags)
+let libPath = Sys.getenv "OPENSSL_LIB_PATH" in
+let includePath = Sys.getenv "OPENSSL_INCLUDE_PATH" in
+
+let libs = ["-L" ^ libPath; "-lssl"; "-lcrypto"] in
+let cflags = ["-I" ^ includePath] in
+
+C.Flags.write_sexp "c_library_flags.sexp" libs;
+C.Flags.write_sexp "c_flags.sexp" cflags;
