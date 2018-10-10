6b0900b9fc69f8a34c198437eda85faa6e84d2d1
Bryan Phelps <bryphe@outlook.com>
2018-10-10 12:12:33 -0700

    Tweak discover file

--- src/config/discover.ml
+++ src/config/discover.ml
@@ -1,10 +1,10 @@
 module C = Configurator.V1
 
-let libPath = Sys.getenv "OPENSSL_LIB_PATH" in
-let includePath = Sys.getenv "OPENSSL_INCLUDE_PATH" in
+let libPath = Sys.getenv "OPENSSL_LIB_PATH";
+let includePath = Sys.getenv "OPENSSL_INCLUDE_PATH";
 
-let libs = ["-L" ^ libPath; "-lssl"; "-lcrypto"] in
-let cflags = ["-I" ^ includePath] in
+let libs = ["-L" ^ libPath; "-lssl"; "-lcrypto"]; in
+let cflags = ["-I" ^ includePath];
 
 C.Flags.write_sexp "c_library_flags.sexp" libs;
 C.Flags.write_sexp "c_flags.sexp" cflags;
