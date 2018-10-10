8210373332bcde0084bc86663ba0ef7cc60a4253
Bryan Phelps <bryphe@outlook.com>
2018-10-10 11:50:19 -0700

    Remove unused variable

--- src/config/discover.ml
+++ src/config/discover.ml
@@ -1,7 +1,7 @@
 module C = Configurator.V1
 
 let () =
-  C.main ~name:"ssl" (fun c ->
+  C.main ~name:"ssl" (fun _c ->
       let libPath = Sys.getenv "OPENSSL_LIB_PATH" in
       let includePath = Sys.getenv "OPENSSL_INCLUDE_PATH" in
       let libs = ["-L" ^ libPath; "-lssl"; "-lcrypto"] in
