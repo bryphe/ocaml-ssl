bf672ed0f440347a1ae08209f5b083eac8f3d17c
Bryan Phelps <bryphe@outlook.com>
2018-10-10 14:34:40 -0700

    Fix discover.ml file

--- src/config/discover.ml
+++ src/config/discover.ml
@@ -1,10 +1,11 @@
 module C = Configurator.V1
 
-let libPath = Sys.getenv "OPENSSL_LIB_PATH";
-let includePath = Sys.getenv "OPENSSL_INCLUDE_PATH";
+let () =
+    let libPath = Sys.getenv "OPENSSL_LIB_PATH" in
+    let includePath = Sys.getenv "OPENSSL_INCLUDE_PATH" in
 
-let libs = ["-L" ^ libPath; "-lssl"; "-lcrypto"]; in
-let cflags = ["-I" ^ includePath];
+    let libs = ["-L" ^ libPath; "-lssl"; "-lcrypto"] in
+    let cflags = ["-I" ^ includePath] in
 
-C.Flags.write_sexp "c_library_flags.sexp" libs;
-C.Flags.write_sexp "c_flags.sexp" cflags;
+    C.Flags.write_sexp "c_library_flags.sexp" libs;
+    C.Flags.write_sexp "c_flags.sexp" cflags;
